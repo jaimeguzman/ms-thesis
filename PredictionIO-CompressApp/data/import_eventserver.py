c3f6fdfe319447a6ba9573c6601740478620d235
Jaime Guzman <jguzman@ratslabs.io>
2015-08-27 10:30:07 -0300

    Modelo de eventos-features para importar data en python

--- PredictionIO-CompressApp/data/import_eventserver.py
+++ PredictionIO-CompressApp/data/import_eventserver.py
@@ -6,32 +6,30 @@ import predictionio
 import argparse
 
 # def import_events(client, file):
-def import_events(file):
+def import_events(client, file):
   f = open(file, 'r')
   count = 0
   print "Importing data..."
   for line in f:
     data = line.rstrip('\r\n').split(" ")
+    data.remove('')
 
     # print data
+    cliente = client  
+    pos = 0
+    for feature in data:
+      pos += 1
+      print "El usuario %d vio la pagina %s por %d vez " % (count,feature, pos)
+      cliente.create_event(
+        event="view", #el usuario siempre ve una seccion de PRISA
+        entity_type="user",
+        entity_id=str(count), # use the count num as user ID
+        properties= {
+          "page" : feature,
+          "pos" :  int(pos)
+        }
+      )
 
-    plan = data[0]
-    # attr = data[1].split(" ")
-
-    print plan
-
-
-    # client.create_event(
-    #   event="view", #el usuario siempre ve una seccion de PRISA
-    #   entity_type="user",
-    #   entity_id=str(count), # use the count num as user ID
-    #   properties= {
-    #     "attr0" : int(attr[0]),
-    #     "attr1" : int(attr[1]),
-    #     "attr2" : int(attr[2]),
-    #     "plan" : int(plan)
-    #   }
-    # )
     count += 1
   f.close()
   print "%s events are imported." % count
@@ -39,17 +37,16 @@ def import_events(file):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(
     description="Import sample data for classification engine")
-  # parser.add_argument('--access_key', default='invald_access_key')
+  parser.add_argument('--access_key', default='invald_access_key')
   parser.add_argument('--url', default="http://localhost:7070")
   parser.add_argument('--file', default="msnbc990928-letter-split10.seq")
 
   args = parser.parse_args()
   print args
 
-  # client = predictionio.EventClient(
-  #   # access_key=args.access_key,
-  #   url=args.url,
-  #   threads=5,
-  #   qsize=500)
-  # import_events(client, args.file)
-  import_events(args.file)
+  client = predictionio.EventClient(
+    access_key=args.access_key,
+    url=args.url,
+    threads=5,
+    qsize=500)
+  import_events(client, args.file)
